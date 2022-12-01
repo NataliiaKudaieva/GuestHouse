3b64130209cedfcac7e2ebb912b886b50fbce706
Nataliia Kudaieva <natkudaieva@gmail.com>
2022-12-01 22:18:31 +0200

    stop js animation

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 //////guests section
 const prev = document.querySelector(".guests__prev");
 const next = document.querySelector(".guests__next");
+const guests = document.querySelectorAll(".guests__item");
 const guest1 = document.getElementById("guests__item--1");
 const guest2 = document.getElementById("guests__item--2");
 const guest3 = document.getElementById("guests__item--3");
 
+function stopAnimation() {
+  guests.forEach((list) => list.classList.add("paused"));
+}
+window.addEventListener("click", stopAnimation);
+
 next.addEventListener("click", () => {
-  guest2.style.left = "-50%";
-  guest3.style.left = "-50%";
+  guest1.style.transform = "translateX(0px)";
+  guest2.style.transform = "translateX(-70%)";
+  guest3.style.transform = "translateX(-50%)";
 });
 
 prev.addEventListener("click", () => {
-  guest2.style.left = "0%";
-  guest3.style.left = "0%";
+  guest2.style.transform = "translateX(0px)";
+  guest3.style.transform = "translateX(0px)";
 });
