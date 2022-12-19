d667302c0879a8ed3415a8ed3fdb4df8e065b7b8
Nataliia Kudaieva <natkudaieva@gmail.com>
2022-12-19 23:11:25 +0200

    fixed slider

--- js/script.js
+++ js/script.js
@@ -2,23 +2,21 @@
 const prev = document.querySelector(".guests__prev");
 const next = document.querySelector(".guests__next");
 const guests = document.querySelectorAll(".guests__item");
-const guest1 = document.getElementById("guests__item--1");
-const guest2 = document.getElementById("guests__item--2");
-const guest3 = document.getElementById("guests__item--3");
 
-function stopAnimation() {
-  guests.forEach((list) => list.classList.add("paused"));
-}
-window.addEventListener("click", stopAnimation);
+let offset = 0;
 
-next.addEventListener("click", () => {
-  guest1.style.transform = "translateX(-10%)";
-  guest2.style.transform = "translateX(-80%)";
-  guest3.style.transform = "translateX(-80%)";
+next.addEventListener("click", function () {
+  offset = offset + 50;
+  if (offset > 70) {
+    offset = 0;
+  }
+  guests.forEach((el) => (el.style.left = -offset + "%"));
 });
 
-prev.addEventListener("click", () => {
-  guest1.style.transform = "translateX(30%)";
-  guest2.style.transform = "translateX(30%)";
-  guest3.style.transform = "translateX(100%)";
+prev.addEventListener("click", function () {
+  offset = offset - 50;
+  if (offset < 70) {
+    offset = 0;
+  }
+  guests.forEach((el) => (el.style.left = -offset + "%"));
 });
